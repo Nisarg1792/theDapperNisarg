1f5cbef77a035d266f20b72657aef030404254d9
Nisarg Patel <nisarg1792@gmail.com>
2019-05-03 08:55:34 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "May 1, 2019 18:03:26",
+	date: "May 10, 2019 18:03:26",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
